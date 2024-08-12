478f9a6975103e7a839964d74153e393445ba214
Dhruv <dhruvmitna9@gmail.com>
2024-08-12 17:02:18 +0530

    Added zeta(4).

--- Zeta Function/zeta(4)/zeta4.py
+++ Zeta Function/zeta(4)/zeta4.py
@@ -1,19 +1,16 @@
-# TODO: Implement more ramanujan-sato algorithms and move them into a separate folder if there are enough different series to justify doing so.
 from decimal import Decimal
-
-def factorial(n: int):
-    return factorial(n - 1) * n if n != 0 else 1
 
 pi: Decimal = Decimal(3.141592653589793238462643383279502884197169399375) # math.pi has less decimal places, and this is the highest number of decimal places I could get python to print.
 
-k: int = 0
-approximationInverted: Decimal = 0
+k: int = 1
+approximationExponentiated: Decimal = 0
+approximation: Decimal = 0
 previous: Decimal = 0
 
 while True:
 
-    approximationInverted += Decimal((factorial(4*k) * (26390*k + 1103)) / (pow(factorial(k), 4) * pow(396, 4*k)))
-    approximation: Decimal = Decimal(9801 / (2 * Decimal(pow(2, 1/2)) * (approximationInverted)))
+    approximationExponentiated += 90 / pow(k, 4)
+    approximation = Decimal(pow(approximationExponentiated, 1/4))
 
     print(f"\nIteration {k + 1}")
     print(f"Approximation = {approximation}")
