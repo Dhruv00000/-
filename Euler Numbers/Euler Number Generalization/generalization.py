1217259a68c5f1d2e31fdc1984cf0866125d0efa
Dhruv <dhruvmitna9@gmail.com>
2024-08-14 13:13:02 +0530

    Added a generalization for Euler number based algorithms.

--- Euler Numbers/Euler Number Generalization/generalization.py
+++ Euler Numbers/Euler Number Generalization/generalization.py
@@ -2,7 +2,7 @@ from decimal import Decimal
 
 pi: Decimal = Decimal(3.141592653589793238462643383279502884197169399375) # math.pi has less decimal places, and this is the highest number of decimal places I could get python to print.
 
-k: int = 1
+n: int = 0
 approximationExponentiated: float = 0
 approximation: Decimal = 0
 previous: Decimal = 0
@@ -10,29 +10,34 @@ previous: Decimal = 0
 def factorial(n: int):
     return factorial(n - 1) * n if n != 0 else 1
 
-try: n: int = int(input("Enter a value for 'n': "))
-except ValueError: n = "a" # Setting 'n' to a non-integer ensures that the below check fails and the execution flow is transferred to the else statement at the bottom.
+try: k: int = int(input("Enter a value for 'k': "))
+except ValueError: k = "a" # Setting 'n' to a non-integer ensures that the below check fails and the execution flow is transferred to the else statement at the bottom.
 
-def Bernoulli(num: int):
+def Euler_2k(num: int):
+
     if num == 0: return 1
+    result: int = 0
 
-    result: float = sum(
-        (Bernoulli(l) * factorial(num))
-        / (factorial(l) * factorial(num - l) * (num - l + 1))
-        for l in range(num)
-    )
-    return 1 - result
+    for k in range(1, 2*num + 1):
+        innerLoopSum: float = sum(
+            (pow(-1, l) * pow(k - l, 2 * num) * factorial(2 * k))
+            / (factorial(l) * factorial(2 * k - l))
+            for l in range(2 * k + 1)
+        )
+        result += pow(-1, k) * pow(2, -k) * innerLoopSum
 
-if isinstance(n, int) and n != 0 and n > 0:
+    return result
+
+if isinstance(k, int) and k >= 0:
 
     while True:
-        try: approximationExponentiated += pow(-1, n + 1) * (2 * factorial(2*n)) / (pow(k, 2*n) * pow(2, 2*n) * Bernoulli(2*n))
+        try: approximationExponentiated += (pow(-1, n-k) * factorial(2*k) * pow(2, 2*k + 2)) / (pow(2*n + 1, 2*k + 1) * Euler_2k(k))
         except OverflowError:
             print("The entered value is too large to handle.")
             break
-        approximation = Decimal(pow(approximationExponentiated, 1 / (2*n)))
+        approximation = Decimal(pow(approximationExponentiated, 1 / (2*k + 1)))
 
-        print(f"\nIteration {k}")
+        print(f"\nIteration {n + 1}")
         print(f"Approximation = {approximation}")
 
         for i, char in enumerate("3.141592653589793238462643383279502884197169399375"): # using str(pi) instead of writing the whole string like I have done here somehow displays a different number??? idk
@@ -49,7 +54,7 @@ if isinstance(n, int) and n != 0 and n > 0:
         elif deviation != approximation: print(f"Deviation from previous iteration: {approximation - previous}") # this if-statement prevents a deviation from being shown during the first iteration.
 
         previous = approximation
-        k += 1
+        n += 1
 
 else:
-    print("\nn must be a positive integer.\n")
+    print("\nn must be a whole number.\n")
