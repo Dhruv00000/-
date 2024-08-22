5e3a4eec8214a4f88d24a3f8cc92547fb1655396
Dhruv <dhruvmitna9@gmail.com>
2024-08-22 18:16:58 +0530

    Added the Nilkantha series.

---
+++ Other Series/Nilkantha Series/nilkantha.py
@@ -0,0 +1,43 @@
+from decimal import Decimal
+from time import perf_counter
+
+pi: Decimal = Decimal(3.141592653589793238462643383279502884197169399375) # math.pi has less decimal places, and this is the highest number of decimal places I could get python to print.
+
+k: int = 1
+approximationIntermediate: Decimal = 0
+approximation: Decimal = 0
+previous: Decimal = 0
+finalAccuracy: int = 0
+totalComputationTime: float = 0
+
+while True:
+
+    iterationStartTime: float = perf_counter()
+    approximationIntermediate += Decimal(pow(-1, k + 1) / (k * (k + 1) * (2*k + 1)))
+    approximation = approximationIntermediate + 3
+    iterationEndTime: float = perf_counter()
+
+    print(f"\nIteration {k}")
+    print(f"Approximation = {approximation}")
+
+    for i, char in enumerate("3.141592653589793238462643383279502884197169399375"): # using str(pi) instead of writing the whole string like I have done here somehow displays a different number??? idk
+        if char != str(approximation)[i]:
+            if i < 2: print("No accurate decimal places")
+            else:
+                print(f"{i - 2} correct decimal place(s)")
+                finalAccuracy = i - 2
+            break
+
+    print(f"Iteration duration: {iterationEndTime - iterationStartTime}  seconds")
+    totalComputationTime += iterationEndTime - iterationStartTime
+
+    deviation: Decimal = approximation - previous
+    if deviation == 0:
+        print("Negligible deviation (terminating the program)\n")
+        break
+    elif deviation != approximation: print(f"Deviation from previous iteration: {approximation - previous}") # this if-statement prevents a deviation from being shown during the first iteration.
+
+    previous = approximation
+    k += 1
+
+print(f"\n\nComputed {finalAccuracy} correct decimal places in {totalComputationTime} seconds and {k} iterations.\n")
