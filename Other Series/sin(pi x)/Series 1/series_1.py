d53c2a4f36552b451b3d1a54b647bbcad6a947f3
Dhruv <dhruvmitna9@gmail.com>
2024-08-28 18:12:04 +0530

    Added two new algorithms.

---
+++ Other Series/sin(pi x)/Series 1/series_1.py
@@ -0,0 +1,47 @@
+from decimal import Decimal
+from time import perf_counter
+
+pi: Decimal = Decimal(3.141592653589793238462643383279502884197169399375) # math.pi has less decimal places, and this is the highest number of decimal places I could get python to print.
+
+k: int = 0
+approximation: Decimal = 0
+previous: Decimal = 0
+finalAccuracy: int = 0
+totalComputationTime: float = 0
+
+def approximationFunction(num: int):
+    return Decimal(pow(-1, num) / (num + 1/2))
+
+while True:
+
+    iterationStartTime: float = perf_counter()
+    approximation += approximationFunction(k)
+    if k != 0: approximation += approximationFunction(-k)
+    iterationEndTime: float = perf_counter()
+
+    print(f"\nIteration {k + 1}")
+    print(f"Approximation = {approximation}")
+
+    for i, char in enumerate("3.141592653589793238462643383279502884197169399375"): # using str(pi) instead of writing the whole string like I have done here somehow displays a different number??? idk
+        try:
+            if char != str(approximation)[i]:
+                if i < 2: print("No accurate decimal places")
+                else:
+                    print(f"{i - 2} correct decimal place(s)")
+                    finalAccuracy = i - 2
+                break
+        except IndexError: print("No incorrect decimal places.")
+
+    print(f"Iteration duration: {iterationEndTime - iterationStartTime}  seconds")
+    totalComputationTime += iterationEndTime - iterationStartTime
+
+    deviation: Decimal = approximation - previous
+    if deviation == 0: 
+        print("Negligible deviation (terminating the program)\n")
+        break
+    elif deviation != approximation: print(f"Deviation from previous iteration: {deviation}") # this if-statement prevents a deviation from being shown during the first iteration.
+
+    previous = approximation
+    k += 1
+
+print(f"\n\nComputed {finalAccuracy} correct decimal places in {totalComputationTime} seconds and {k + 1} iterations.\n")
