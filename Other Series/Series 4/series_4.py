a265973a59c698bfbb5585ec6193b26cdb35a8b0
Dhruv <dhruvmitna9@gmail.com>
2024-09-04 17:54:35 +0530

    Added a new algorithm.

--- Other Series/Series 4/series_4.py
+++ Other Series/Series 4/series_4.py
@@ -1,31 +1,23 @@
 from decimal import Decimal
 from time import perf_counter
+from math import tan
+from time import sleep
 
-def sum_of_divisors(num: int):
-    divisorSum: int = sum(
-        divisorCandidate
-        for divisorCandidate in range(1, num)
-        if num % divisorCandidate == 0
-    )
-    return divisorSum + num
-
-e: float = 2.7182818284590452
-
-k: int = 1
-approximationIntermediate: float = 0
+k: int = 2
+approximationInverted: float = 0
 approximation: Decimal = 0
-previous: Decimal = 3.14
+previous: Decimal = 3.14159265358979323
 finalAccuracy: int = 0
 totalComputationTime: float = 0
 
 while True:
 
     iterationStartTime: float = perf_counter()
-    approximationIntermediate += sum_of_divisors(k) * pow(e, (-2 * float(previous) * k))
-    approximation = 1 / Decimal(1/3 - 8 * approximationIntermediate)
+    approximationInverted += tan(previous / pow(2, k)) / pow(2, k)
+    approximation = 1 / Decimal(approximationInverted)
     iterationEndTime: float = perf_counter()
 
-    print(f"\nIteration {k}")
+    print(f"\nIteration {k - 1}")
     print(f"Approximation = {approximation}")
 
     for i, char in enumerate("3.141592653589793238462643383279502884197169399375"): # using str(pi) instead of writing the whole string like I have done here somehow displays a different number??? idk
@@ -40,12 +32,14 @@ while True:
     totalComputationTime += iterationEndTime - iterationStartTime
 
     deviation: Decimal = approximation - Decimal(previous)
-    if deviation == 0: 
-        print("Negligible deviation (terminating the program)\n")
-        break
+    if deviation == 0 and previous != 3.14: 
+            print("Negligible deviation (terminating the program)\n")
+            break
     elif deviation != approximation: print(f"Deviation from previous iteration: {deviation}") # this if-statement prevents a deviation from being shown during the first iteration.
+
+    if finalAccuracy >= 2: previous = approximation # this stops the extremely incorrect approximations from the first few steps from ruining all succeeding steps.
 
     previous = approximation
     k += 1
 
-print(f"\n\nComputed {finalAccuracy} correct decimal places in {totalComputationTime} seconds and {k} iterations.\n")
+print(f"\n\nComputed {finalAccuracy} correct decimal places in {totalComputationTime} seconds and {k - 1} iterations.\n")
